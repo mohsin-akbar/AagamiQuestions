257a5fbf8b3a09dd53af51b8ce4dfe2db81d7205
User <User@DESKTOP-9AD633G>
2023-04-02 17:40:11 +0530

    AgamiQuestion commit

---
+++ src/test/java/com/agami/AgamiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgamiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
